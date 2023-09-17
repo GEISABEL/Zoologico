b6870dc853cf2b8acbe007ac302740709a7084d9
GEISABEL <110138843+GEISABEL@users.noreply.github.com>
2023-09-17 00:34:06 -0300

    Att

---
+++ Elefante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoologico
+{
+    public class Elefante : Animal
+    {
+        public override void EmitirSom()
+        {
+            Console.WriteLine("Barulho de Elefante");
+        }
+
+    }
+}
